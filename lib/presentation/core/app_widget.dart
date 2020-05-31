e19c9b82d0c9fe4cd178b81fd43dc94f15a49a97
Amareesh <amareesh@three38inc.com>
2020-05-31 13:46:22 +0530

    cught up till part 10

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,20 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:ddd_app/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../application/auth/auth_bloc.dart';
+import '../../injection.dart';
 import '../sign_in/sign_in_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Note App',
-      home: SignInPage(),
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.green[800],
-        accentColor: Colors.blueAccent,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Note App',
+        builder: ExtendedNavigator(router: Router()),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
         ),
       ),
---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:ddd_app/application/auth/auth_bloc.dart';
+import 'package:ddd_app/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.map(
+          initial: (_) {},
+          authenticated: (_) {
+            print('I am Authenticated!');
+          },
+          unauthenticated: (_) => ExtendedNavigator.of(context)
+              .pushReplacementNamed(Routes.signInPage),
+        );
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
