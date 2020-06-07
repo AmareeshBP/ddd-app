a941ab9f645a5abf2c42cdd603871136785fdbc5
Amareesh <amareesh@three38inc.com>
2020-06-07 21:01:33 +0530

    caught up to 12

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,6 +1,32 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
 
 import 'failures.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) return Right(input);
+  return Left(ValueFailure.exeedingLength(failedValue: input, max: maxLength));
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) return Right(input);
+  return Left(ValueFailure.empty(failedValue: input));
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (input.contains('\n'))
+    return Left(ValueFailure.multiLine(failedValue: input));
+  return Right(input);
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+    KtList<T> input, int maxLength) {
+  if (input.size <= maxLength) return Right(input);
+  return Left(ValueFailure.exeedingLength(failedValue: input, max: maxLength));
+}
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const String emailRegex =
